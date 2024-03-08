140056f5387fda65f15f5253b8f1ad6a4c3dc5e7
CarterLeeW <103856985+CarterLeeW@users.noreply.github.com>
2024-03-08 12:38:01 -0500

    57 UI widget data table

--- Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
@@ -4,14 +4,32 @@
 
 #include "CoreMinimal.h"
 #include "UI/WidgetController/AuraWidgetController.h"
+#include "GameplayTagContainer.h"
+#include "Engine/DataTable.h"
 #include "OverlayWidgetController.generated.h"
 
 struct FOnAttributeChangeData;
+class UAuraUserWidget;
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnHealthChangedSignature, float, NewHealth);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxHealthChangedSignature, float, NewMaxHealth);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnManaChangedSignature, float, NewMana);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxManaChangedSignature, float, NewMaxMana);
+
+USTRUCT(BlueprintType)
+struct FUIWidgetRow : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	FGameplayTag MessageTag = FGameplayTag();
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	FText Message = FText();
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	TSubclassOf<UAuraUserWidget> MessageWidget;
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+	UTexture2D* Image = nullptr;
+};
 
 /**
  * 
@@ -38,6 +56,9 @@ public:
 	FOnMaxManaChangedSignature OnMaxManaChanged;
 
 protected:
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category = "Widget Data")
+	TObjectPtr<UDataTable> MessageWidgetDataTable;
+
 	void HealthChanged(const FOnAttributeChangeData& Data) const;
 	void MaxHealthChanged(const FOnAttributeChangeData& Data) const;
 	void ManaChanged(const FOnAttributeChangeData& Data) const;
