f78d6061dc6a31d3e6d04d751d8ebbc0c6b69cd7
CarterLeeW <103856985+CarterLeeW@users.noreply.github.com>
2024-03-19 16:18:09 -0400

    90 attribute menu widget controller

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,14 @@
+// Copyright Carter Wooton
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BroadCastInitialValues()
+{
+
+}
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+
+}
