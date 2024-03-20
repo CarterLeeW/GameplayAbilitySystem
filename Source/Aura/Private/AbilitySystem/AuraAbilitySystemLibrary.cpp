4ebcfb5d282d784e3220f91c44cb7070b33e6fa9
CarterLeeW <103856985+CarterLeeW@users.noreply.github.com>
2024-03-20 12:21:10 -0400

    91 Aura ability system blueprint library

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemLibrary.cpp
@@ -0,0 +1,24 @@
+// Copyright Carter Wooton
+
+
+#include "AbilitySystem/AuraAbilitySystemLibrary.h"
+#include "UI/WidgetController/OverlayWidgetController.h"
+#include "UI/HUD/AuraHUD.h"
+#include "Player/AuraPlayerState.h"
+
+UOverlayWidgetController* UAuraAbilitySystemLibrary::GetOverlayWidgetController(const UObject* WorldContextObject)
+{
+	APlayerController* PC = WorldContextObject->GetWorld()->GetFirstPlayerController();
+	if (PC)
+	{
+		if (AAuraHUD* AuraHUD = Cast<AAuraHUD>(PC->GetHUD()))
+		{
+			AAuraPlayerState* PS = PC->GetPlayerState<AAuraPlayerState>();
+			UAbilitySystemComponent* ASC = PS->GetAbilitySystemComponent();
+			UAttributeSet* AS = PS->GetAttributeSet();
+			const FWidgetControllerParams WidgetControllerParams(PC, PS, ASC, AS);
+			return AuraHUD->GetOverlayWidgetController(WidgetControllerParams);
+		}
+	}
+	return nullptr;
+}
