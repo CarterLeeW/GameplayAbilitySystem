dc78c5b44327605cd41f58b8518fac03573dda98
CarterLeeW <103856985+CarterLeeW@users.noreply.github.com>
2024-06-01 18:28:34 -0400

    249 level up info data asset

---
+++ Source/Aura/Private/AbilitySystem/Data/LevelUpInfo.cpp
@@ -0,0 +1,18 @@
+// Copyright Carter Wooton
+
+
+#include "AbilitySystem/Data/LevelUpInfo.h"
+
+int32 ULevelUpInfo::FindLevelForExp(int32 Exp)
+{
+	/* LevelUpInformation[0] is just a placeholder. Useful information begins at index 1 */
+	for (int32 i = 1; i < LevelUpInformation.Num(); i++)
+	{
+		if (Exp < LevelUpInformation[i].LevelUpRequirement)
+		{
+			return i;
+		}
+	}
+
+	return 1;
+}
