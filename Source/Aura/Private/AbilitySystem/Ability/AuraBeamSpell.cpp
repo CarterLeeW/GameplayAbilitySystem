3b951ed8c9cae5a7fd228563f5f5d93c4be6f72f
CarterLeeW <103856985+CarterLeeW@users.noreply.github.com>
2024-06-25 13:39:41 -0400

    320 aura beam spell

---
+++ Source/Aura/Private/AbilitySystem/Ability/AuraBeamSpell.cpp
@@ -0,0 +1,17 @@
+// Copyright Carter Wooton
+
+
+#include "AbilitySystem/Ability/AuraBeamSpell.h"
+
+void UAuraBeamSpell::CacheMouseDataInfo(const FHitResult& HitResult)
+{
+	if (HitResult.bBlockingHit)
+	{
+		MouseHitLocation = HitResult.ImpactPoint;
+		MouseHitActor = HitResult.GetActor();
+	}
+	else
+	{
+		CancelAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true);
+	}
+}
---
+++ Source/Aura/Public/AbilitySystem/Ability/AuraBeamSpell.h
@@ -0,0 +1,31 @@
+// Copyright Carter Wooton
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Ability/AuraDamageGameplayAbility.h"
+#include "AuraBeamSpell.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraBeamSpell : public UAuraDamageGameplayAbility
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable)
+	void CacheMouseDataInfo(const FHitResult& HitResult);
+
+protected:
+
+	UPROPERTY(BlueprintReadWrite, Category = "Beam")
+	FVector MouseHitLocation;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Beam")
+	TObjectPtr<AActor> MouseHitActor;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Beam")
+	TObjectPtr<APlayerController> OwnerPlayerController;
+};
