e8e1ec62a03b9730ed23dda7719d2f4d341f9ab5
CarterLeeW <103856985+CarterLeeW@users.noreply.github.com>
2024-07-17 12:58:37 -0400

    363 fireblast cost and cooldown

--- Source/Aura/Private/AbilitySystem/Ability/AuraElectrocute.cpp
+++ Source/Aura/Private/AbilitySystem/Ability/AuraElectrocute.cpp
@@ -5,7 +5,7 @@
 
 FString UAuraElectrocute::GetDescription(int32 Level) const
 {
-	const float ManaCost = FMath::Abs(GetManaCost(Level));
+	const float ManaCostPerSecond = DamagePeriod > 0.f ? FMath::Abs(GetManaCost(Level) / DamagePeriod) : 0.f;
 	const float Cooldown = GetCooldown(Level);
 	const float DamagePerSecond = DamagePeriod > 0.f ? Damage.GetValueAtLevel(Level) / DamagePeriod : 0.f;
 	return FString::Printf(TEXT(
@@ -13,18 +13,18 @@ FString UAuraElectrocute::GetDescription(int32 Level) const
 		"<Title>ELECTROCUTE</>\n\n"
 		// Details
 		"<Default>Level: %d</>\n"
-		"<Small>Mana Cost: </><ManaCost>%.1f</>\n"
+		"<Small>Mana Cost: </><ManaCost>%.1f</><Small>/s</>\n"
 		"<Small>Cooldown: </><Cooldown>%.1f</>\n"
 		// Description
 		"<Default>Creates a shock chain, dealing </>"
 		"<Damage>%d </>"
 		"<Default>lightning damage per second to a maximum of %d targets with a chance to stun</>\n\n"),
-		Level, ManaCost, Cooldown, FMath::RoundToInt(DamagePerSecond), MaxShockTargets.AsInteger(Level));
+		Level, ManaCostPerSecond, Cooldown, FMath::RoundToInt(DamagePerSecond), MaxShockTargets.AsInteger(Level));
 }
 
 FString UAuraElectrocute::GetNextLevelDescription(int32 Level) const
 {
-	const float ManaCost = FMath::Abs(GetManaCost(Level+1));
+	const float ManaCostPerSecond = DamagePeriod > 0.f ? FMath::Abs(GetManaCost(Level+1) / DamagePeriod) : 0.f;
 	const float Cooldown = GetCooldown(Level+1);
 	const float DamagePerSecond = DamagePeriod > 0.f ? Damage.GetValueAtLevel(Level+1) / DamagePeriod : 0.f;
 	return FString::Printf(TEXT(
@@ -32,11 +32,11 @@ FString UAuraElectrocute::GetNextLevelDescription(int32 Level) const
 		"<Title>NEXT LEVEL</>\n\n"
 		// Details
 		"<Default>Level: %d</>\n"
-		"<Small>Mana Cost: </><ManaCost>%.1f</>\n"
+		"<Small>Mana Cost: </><ManaCost>%.1f</><Small>/s</>\n"
 		"<Small>Cooldown: </><Cooldown>%.1f</>\n"
 		// Description
 		"<Default>Creates a shock chain, dealing </>"
 		"<Damage>%d </>"
 		"<Default>lightning damage per second to a maximum of %d targets with a chance to stun</>\n\n"),
-		Level+1, ManaCost, Cooldown, FMath::RoundToInt(DamagePerSecond), MaxShockTargets.AsInteger(Level+1));
+		Level+1, ManaCostPerSecond, Cooldown, FMath::RoundToInt(DamagePerSecond), MaxShockTargets.AsInteger(Level+1));
 }
