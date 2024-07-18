dd809817be84f749c168c8570fc493bc416b62a4
CarterLeeW <103856985+CarterLeeW@users.noreply.github.com>
2024-07-18 14:12:49 -0400

    367 fireball timelines
    using gameplay cues and homing projectile in BP. may need to change this soon

--- Source/Aura/Private/Actor/AuraFireBall.cpp
+++ Source/Aura/Private/Actor/AuraFireBall.cpp
@@ -8,6 +8,11 @@ void AAuraFireBall::BeginPlay()
 	Super::BeginPlay();
 }
 
+void AAuraFireBall::Destroyed()
+{
+
+}
+
 void AAuraFireBall::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 
--- Source/Aura/Public/Actor/AuraProjectile.h
+++ Source/Aura/Public/Actor/AuraProjectile.h
@@ -31,6 +31,7 @@ public:
 protected:
 	virtual void BeginPlay() override;
 	virtual void Destroyed() override;
+	UFUNCTION()
 	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
 	UFUNCTION(BlueprintCallable)
