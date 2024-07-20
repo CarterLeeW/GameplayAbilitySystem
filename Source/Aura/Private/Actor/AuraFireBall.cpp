0b6af86bb5ed288cfdd6b7cefb83eb5231103a3a
CarterLeeW <103856985+CarterLeeW@users.noreply.github.com>
2024-07-20 14:01:54 -0400

    368 causing fireball damage
    currently, the fireball overlaps many times. This will be fixed soon, depending on the desired effect

--- Source/Aura/Private/Actor/AuraFireBall.cpp
+++ Source/Aura/Private/Actor/AuraFireBall.cpp
@@ -1,7 +1,9 @@
 // Copyright Carter Wooton
 
-
 #include "Actor/AuraFireBall.h"
+#include "AbilitySystemComponent.h"
+#include "AbilitySystem/AuraAbilitySystemLibrary.h"
+#include "AbilitySystemBlueprintLibrary.h"
 
 void AAuraFireBall::BeginPlay()
 {
@@ -15,5 +17,24 @@ void AAuraFireBall::Destroyed()
 
 void AAuraFireBall::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
+	if (!DamageEffectParams.SourceAbilitySystemComponent) return;
+	const AActor* SourceAvatarActor = DamageEffectParams.SourceAbilitySystemComponent->GetAvatarActor();
+	if (SourceAvatarActor == OtherActor) return; // Do not hit yourself
 
+	// This disables friendly fire
+	if (SourceAvatarActor && !UAuraAbilitySystemLibrary::IsNotFriend(SourceAvatarActor, OtherActor))
+	{
+		return;
+	}
+
+	if (HasAuthority())
+	{
+		// Apply any gameplay effects
+		if (UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(OtherActor))
+		{
+			DamageEffectParams.DeathImpulse = GetActorForwardVector() * DamageEffectParams.DeathImpulseMagnitude;
+			DamageEffectParams.TargetAbilitySystemComponent = TargetASC; // This is the first time TargetASC is valid
+			UAuraAbilitySystemLibrary::ApplyDamageEffect(DamageEffectParams);
+		}
+	}
 }
