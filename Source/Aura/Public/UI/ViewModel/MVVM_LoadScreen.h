8ee5b18b8ac1ac7f122b0ca2a89fc8ec2726f6bc
CarterLeeW <103856985+CarterLeeW@users.noreply.github.com>
2024-07-27 20:39:04 -0400

    384 switching the widget switcher
    need to fix 5.3 problems

--- Source/Aura/Public/UI/ViewModel/MVVM_LoadScreen.h
+++ Source/Aura/Public/UI/ViewModel/MVVM_LoadScreen.h
@@ -20,6 +20,12 @@ public:
 	void InitializeLoadSlots();
 	UFUNCTION(BlueprintPure)
 	UMVVM_LoadSlot* GetLoadSlotViewModelByIndex(int32 Index) const;
+	UFUNCTION(BlueprintCallable)
+	void NewSlotButtonPressed(int32 Slot, const FString& EnteredName);
+	UFUNCTION(BlueprintCallable)
+	void NewGameButtonPressed(int32 Slot);
+	UFUNCTION(BlueprintCallable)
+	void SelectSlotButtonPressed(int32 Slot);
 
 	UPROPERTY(EditDefaultsOnly)
 	TSubclassOf<UMVVM_LoadSlot> LoadSlotViewModelClass;
@@ -28,12 +34,15 @@ private:
 	UPROPERTY()
 	TMap<int32, UMVVM_LoadSlot*> LoadSlots;
 
+	// Vacant
 	UPROPERTY()
 	TObjectPtr<UMVVM_LoadSlot> LoadSlot_0;
 
+	// New Game
 	UPROPERTY()
 	TObjectPtr<UMVVM_LoadSlot> LoadSlot_1;
 
+	// Load Game
 	UPROPERTY()
 	TObjectPtr<UMVVM_LoadSlot> LoadSlot_2;
 };
