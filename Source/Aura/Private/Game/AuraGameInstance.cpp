5e6cb8aa44ecbce1ef239e6edc406a607c63b3cd
CarterLeeW <103856985+CarterLeeW@users.noreply.github.com>
2024-08-05 12:16:46 -0400

    396.1

---
+++ Source/Aura/Private/Game/AuraGameInstance.cpp
@@ -0,0 +1,5 @@
+// Copyright Carter Wooton
+
+
+#include "Game/AuraGameInstance.h"
+
---
+++ Source/Aura/Public/Game/AuraGameInstance.h
@@ -0,0 +1,17 @@
+// Copyright Carter Wooton
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "AuraGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
