1050b91e16a58925d6c1f142646c44ba3e8cf2d0
CarterLeeW <103856985+CarterLeeW@users.noreply.github.com>
2024-08-09 21:33:59 -0400

    410 different highlight colors

--- Source/Aura/Aura.h
+++ Source/Aura/Aura.h
@@ -5,6 +5,8 @@
 #include "CoreMinimal.h"
 
 #define CUSTOM_DEPTH_RED 250
+#define CUSTOM_DEPTH_BLUE 251
+#define CUSTOM_DEPTH_TAN 252
 #define ECC_Projectile ECollisionChannel::ECC_GameTraceChannel1
 #define ECC_CursorVis ECollisionChannel::ECC_GameTraceChannel2
 #define ECC_OccludingEnvironment ECollisionChannel::ECC_GameTraceChannel3
